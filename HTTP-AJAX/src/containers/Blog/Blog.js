31b5ef892b97e592cb15ef6ce80717f976e1a884
Joe Bugajski <yogurtmystery@gmail.com>
2019-03-06 00:11:01 -0600

    pulled in some data from our fake server

--- HTTP-AJAX/src/containers/Blog/Blog.js
+++ HTTP-AJAX/src/containers/Blog/Blog.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map( post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({
+                    posts: updatedPosts
+                })
+            });
+    }
+
     render () {
+        const posts = this.state.posts.map( post => {
+            return <Post key={post.id}title={post.title} author={post.author}/>
+        });
         return (
             <div>
                 <section className="Posts">
